9a0bd97e2a67a855bb9d8a346ba771e4baf3c28a
Aghosh <aghosh@gmail.com>
2026-03-12 01:21:49 +0530

    fix: Service Api endpoint changed

--- frontend/src/Pages/Services/Api.jsx
+++ frontend/src/Pages/Services/Api.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const Api = axios.create({
-    baseURL:"http://localhost:8000/api",
-    withCredentials:true,
-   
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+    withCredentials: true,
 })
 export default Api
